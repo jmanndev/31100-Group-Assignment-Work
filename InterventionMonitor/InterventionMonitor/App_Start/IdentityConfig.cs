949bcfbbf9ac6e9fc7d8182acad6aeffabd164f6
Jenny Nguyen <knockycode@progsoc.org>
2017-04-03 13:05:13 +1000

    Add .gitignore file and lessen strength strictness of new password

--- InterventionMonitor/InterventionMonitor/App_Start/IdentityConfig.cs
+++ InterventionMonitor/InterventionMonitor/App_Start/IdentityConfig.cs
@@ -50,10 +50,10 @@ namespace InterventionMonitor
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
