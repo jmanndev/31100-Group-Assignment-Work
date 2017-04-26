7f84b3db803614205a75681453144652d0fd67eb
Minijal <geodu3@gmail.com>
2017-04-26 23:33:36 +1000

    added reporting page and roles
    reports page added only accessible by an accountant - the page itself is
    not yet functional

--- InterventionMonitor/InterventionMonitor/App_Start/Startup.Auth.cs
+++ InterventionMonitor/InterventionMonitor/App_Start/Startup.Auth.cs
@@ -20,7 +20,7 @@ namespace InterventionMonitor
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
-
+            //app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
             // Configure the sign in cookie
---
+++ InterventionMonitor/InterventionMonitor/ViewReports.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InterventionMonitor
+{
+    public partial class ViewReports : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<string> reports = new List<string>();
+            reports.Add("Total cost by engineer");
+            reports.Add("Average cost by engineer");
+            reports.Add("Cost by District");
+            reports.Add("Monthly costs for district");
+            ddlReports.DataSource = reports;
+            ddlReports.DataBind();
+        }
+    }
+}
