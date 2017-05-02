d847c0970fdf96382d5de00aaca61eee0f9468cc
Jordyz96 <jordan.j.labonne@student.uts.edu.au>
2017-05-02 22:50:05 +1000

    Fixed up previous Unit Test
    Modified the current unit test to prevent using magic numbers

--- InterventionMonitor/InterventionMonitor.Tests/When_a_engineer_views_clients.cs
+++ InterventionMonitor/InterventionMonitor.Tests/When_a_engineer_views_clients.cs
@@ -12,6 +12,8 @@ namespace InterventionMonitor.Tests
         SiteEngineer siteEngineerB;
         SiteEngineer siteEngineerC;
 
+        int clientsInSydney;
+
         [TestInitialize]
         public void Setup()
         {
@@ -25,9 +27,19 @@ namespace InterventionMonitor.Tests
 
             //SiteEngineer at Sydney has created 3 separate clients
             //This count will attempt to match the Assert.AreEqual checker.
+            clientsInSydney = 0;
+
+            for(int i = 0; i < 3; i++)
+            {
+                siteEngineerA.CreateClient("ClientA" + i, null);
+                clientsInSydney++;
+            }
+
+            /*
             siteEngineerA.CreateClient("ClientA1", null);
             siteEngineerA.CreateClient("ClientA2", null);
             siteEngineerA.CreateClient("ClientA3", null);
+            */
 
             siteEngineerB.CreateClient("ClientB1", null);
             siteEngineerB.CreateClient("ClientB2", null);
@@ -38,8 +50,8 @@ namespace InterventionMonitor.Tests
         [TestMethod]
         public void In_their_current_district_then_display_matching_amount_of_clients_in_current_district()
         {
-            int clientsInSydney = siteEngineerA.ReturnMatchingClients();
-            Assert.AreEqual(clientsInSydney, 3);
+            int matchedClients = siteEngineerA.ReturnMatchingClients();
+            Assert.AreEqual(clientsInSydney, matchedClients);
         }
     }
 }
