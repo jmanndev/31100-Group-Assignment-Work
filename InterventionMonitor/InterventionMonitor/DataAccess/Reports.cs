ce5c45dcfc67c84126832c274a9d1a18e1cce8e4
Minijal <geodu3@gmail.com>
2017-05-02 23:25:32 +1000

    Accountant reporting functions
    3/4 of the reports required to be generated and tried to move some of
    the db logic into data access

---
+++ InterventionMonitor/InterventionMonitor/DataAccess/Reports.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace InterventionMonitor.DataAccess
+{
+    public class Reports
+    {
+        SqlConnection connection = DatabaseConnections.DataConnection;
+        string queryString = "";
+
+        public string TotalEngineerCost(string engineerID)
+        {
+            string result = "";
+            queryString = "SELECT SUM(MaterialCost) AS TotalCost FROM Intervention WHERE SiteEngineerId ='" + engineerID + "'";
+            SqlCommand comm = new SqlCommand(queryString, connection);
+            connection.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            while (reader.Read())
+            {
+                result = reader[0].ToString();
+
+            }
+            connection.Close();
+
+            return result;
+        }
+
+        public string AverageEngineerCost(string engineerID)
+        {
+            string result = "";
+            queryString = "SELECT AVG(MaterialCost) AS TotalCost FROM Intervention WHERE SiteEngineerId ='" + engineerID + "'";
+            SqlCommand comm = new SqlCommand(queryString, connection);
+            connection.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            while (reader.Read())
+            {
+                result = reader[0].ToString();
+            }
+            connection.Close();
+            return result;
+        }
+
+        public string TotalDistrictCost(string districtID)
+        {
+            string result = "";
+            queryString = "SELECT SUM(MaterialCost) AS TotalCost FROM Intervention INNER JOIN Client ON Client.Id = Intervention.ClientId  WHERE Client.DistrictId ='" + districtID + "'";
+            SqlCommand comm = new SqlCommand(queryString, connection);
+            connection.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            while (reader.Read())
+            {
+                result = reader[0].ToString();
+            }
+            connection.Close();
+            return result;
+        }
+
+    }
+}
--- InterventionMonitor/InterventionMonitor/ViewReports.aspx.cs
+++ InterventionMonitor/InterventionMonitor/ViewReports.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using InterventionMonitor.DataAccess;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,13 +13,98 @@ namespace InterventionMonitor
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            List<string> reports = new List<string>();
-            reports.Add("Total cost by engineer");
-            reports.Add("Average cost by engineer");
-            reports.Add("Cost by District");
-            reports.Add("Monthly costs for district");
-            ddlReports.DataSource = reports;
-            ddlReports.DataBind();
+            if (!IsPostBack)
+            {
+                List<string> reports = new List<string>();
+                reports.Add("Please select report to view");
+                reports.Add("Total cost by engineer");
+                reports.Add("Average cost by engineer");
+                reports.Add("Cost by district");
+                reports.Add("Monthly costs for district");
+                ddlReports.DataSource = reports;
+                ddlReports.DataBind();
+
+                SqlConnection connection = DatabaseConnections.DataConnection;
+
+                string queryString = "SELECT * FROM District";
+                SqlCommand comm = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+
+                ddlDistrict.DataSource = reader;
+                ddlDistrict.DataTextField = "Name";
+                ddlDistrict.DataValueField = "Id";
+                ddlDistrict.DataBind();
+                connection.Close();
+
+                string queryString2 = "SELECT * FROM Employee INNER JOIN SiteEngineer ON Employee.Id = SiteEngineer.EmployeeId";
+                SqlCommand comm2 = new SqlCommand(queryString2, connection);
+                connection.Open();
+                SqlDataReader reader2 = comm2.ExecuteReader();
+            
+                ddlEngineer.DataSource = reader2;
+                ddlEngineer.DataTextField = "Name";
+                ddlEngineer.DataValueField = "Id";
+                ddlEngineer.DataBind();
+                connection.Close();
+            
+                ddlEngineer.Visible = false;
+                ddlDistrict.Visible = false;
+                lblDistrict.Visible = false;
+                lblEngineer.Visible = false;
+            }
+        }
+
+        protected void btnRunReport_Click(object sender, EventArgs e)
+        {
+            string engineerID = ddlEngineer.SelectedItem.Value;
+            string districtID = ddlDistrict.SelectedItem.Value;
+            Reports report = new Reports();
+
+            if (ddlReports.SelectedItem.ToString().Equals("Total cost by engineer"))
+            {    
+                decimal result = Decimal.Parse(report.TotalEngineerCost(engineerID));
+                lblResultView.Text = Math.Round(result, 2).ToString();
+            }
+            else if(ddlReports.SelectedItem.ToString().Equals("Average cost by engineer"))
+            {
+                decimal result = Decimal.Parse(report.AverageEngineerCost(engineerID));
+                lblResultView.Text = Math.Round(result, 2).ToString();
+            }
+            else if(ddlReports.SelectedItem.ToString().Equals("Cost by district"))
+            {
+                decimal result = Decimal.Parse(report.TotalDistrictCost(districtID));
+                lblResultView.Text = Math.Round(result, 2).ToString();
+            }
+            else if(ddlReports.SelectedItem.ToString().Equals("Monthly costs for district"))
+            {
+
+            }
+        }
+
+        protected void ddlReports_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlReports.SelectedItem.ToString().Equals("Total cost by engineer") | ddlReports.SelectedItem.ToString().Equals("Average cost by engineer"))
+            {
+                ddlEngineer.Visible = true;
+                lblDistrict.Visible = false;
+                lblEngineer.Visible = true;
+                ddlDistrict.Visible = false;
+            }
+            else if (ddlReports.SelectedItem.ToString().Equals("Cost by district") | ddlReports.SelectedItem.ToString().Equals("Monthly costs for district"))
+            {
+                ddlEngineer.Visible = false;
+                lblDistrict.Visible = true;
+                lblEngineer.Visible = false;
+                ddlDistrict.Visible = true;
+            }
+            else
+            {
+                ddlEngineer.Visible = false;
+                lblDistrict.Visible = false;
+                lblEngineer.Visible = false;
+                ddlDistrict.Visible = false;
+            }
         }
     }
 }
