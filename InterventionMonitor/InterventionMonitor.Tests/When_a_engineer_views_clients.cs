6a5d9bf82a11510df8a2b37b2f9c257c37909b9b
Jordyz96 <jordan.j.labonne@student.uts.edu.au>
2017-05-03 02:38:52 +1000

    Jordyz testcase 004 (#16)
    * Added Unit Test
    
    Added a unit test to match the amount of clients located at the same
    district as the site engineer who is viewing the clients.
    
    * Fixed up previous Unit Test
    
    Modified the current unit test to prevent using magic numbers
    
    * Fixed Unit Test Again
    
    The unit test would only work if you tested it by itself rather than
    running all the test, so I fixed it.
    
    * Added Unit Test for Accountant
    
    Added two unit tests for the accountant, both for changing the district
    for a site engineer and a manager.

---
+++ InterventionMonitor/InterventionMonitor.Tests/When_a_engineer_views_clients.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using InterventionMonitor.Models;
+
+namespace InterventionMonitor.Tests
+{
+    [TestClass]
+    public class When_a_engineer_views_clients
+    {
+        SiteEngineer siteEngineerA;
+
+        int clientsInSydney;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            siteEngineerA = new SiteEngineer();
+
+            siteEngineerA.District = Districts.Instance.Sydney;
+
+            clientsInSydney = 0;
+            for(int i = 0; i < Monitor.Instance.clients.Count; i++)
+            {
+                if (Monitor.Instance.clients[i].District == siteEngineerA.District)
+                    clientsInSydney++;
+            }
+            //SiteEngineer at Sydney has created 3 extra separate clients
+            //This count will attempt to match the Assert.AreEqual checker.
+            for (int i = 0; i < 3; i++)
+            {
+                siteEngineerA.CreateClient("ClientA" + i, null);
+                clientsInSydney++;
+            }
+        }
+
+        [TestMethod]
+        public void In_their_current_district_then_display_matching_amount_of_clients_in_current_district()
+        {
+            int matchedClients = siteEngineerA.ReturnMatchingClients();
+            Assert.AreEqual(clientsInSydney, matchedClients);
+
+            
+        }
+    }
+}
